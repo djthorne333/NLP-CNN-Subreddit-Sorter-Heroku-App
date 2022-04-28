219a100ab77c83f62f4c76677190d987a85261d1
David Thorne <datadave333@gmail.com>
2022-04-27 20:19:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -117,9 +117,8 @@ conv_model.embedding.weight.data[PAD_IDX] = torch.zeros(embed_dim)
 ######################################################################################
 
 
-template_dir = os.path.abspath('../Soulify/App/templates/')
-static_dir = os.path.abspath('../Soulify/App/UI/static/')
-app = Flask(__name__, template_folder=template_dir, static_folder=static_dir
+template_dir = os.path.abspath('NLP-CNN-Subreddit-Sorter-Application/templates')
+app = Flask(__name__, template_folder=template_dir)
 conv_model.load_state_dict(torch.load(r'conv_model.pt'))
 
             
